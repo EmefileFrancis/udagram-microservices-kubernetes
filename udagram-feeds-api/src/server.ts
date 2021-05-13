b2add247f9096c7e7614b6b67906e2c35f8a92b4
Francis <emefilefranklin@gmail.com>
2021-05-13 13:18:56 +0100

    added feeds service

---
+++ udagram-feeds-api/src/server.ts
@@ -0,0 +1,9 @@
+import cors from 'cors';
+import express from 'express';
+import {sequelize} from './sequelize';
+
+import {IndexRouter} from './controllers/v0/index.router';
+
+import bodyParser from 'body-parser';
+import {config} from './config/config';
+import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
